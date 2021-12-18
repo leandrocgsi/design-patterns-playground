e693fc2518905efd1fe85c05f3cd6a72f2272611
leandrocgsi <leandrocgsi@gmail.com>
2021-12-18 18:26:32 -0300

    Working with Adapter Pattern

---
+++ structural-design-patterns/adapter-pattern/src/main/java/br/com/erudio/BankCustomer.java
@@ -0,0 +1,41 @@
+package br.com.erudio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BankCustomer extends BankDetails implements CreditCard {
+
+	public void giveBankDetails() {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			System.out.print("Enter the account holder name :");
+			String customername = br.readLine();
+			System.out.print("\n");
+
+			System.out.print("Enter the account number:");
+			long accountNumber = Long.parseLong(br.readLine());
+			System.out.print("\n");
+
+			System.out.print("Enter the bank name :");
+			String bankname = br.readLine();
+
+			setAccountHolderName(customername);
+			setAccountNumber(accountNumber);
+			setBankName(bankname);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getCreditCard() {
+		long accountNumber = getAccountNumber();
+		String accountHolderName = getAccountHolderName();
+		String bname = getBankName();
+
+		return ("The Account number " + accountNumber + " of " + accountHolderName + " in " + bname
+				+ " bank is valid and authenticated for issuing the credit card.");
+	}
+
+}
---
+++ structural-design-patterns/adapter-pattern/src/main/java/br/com/erudio/CreditCard.java
@@ -0,0 +1,7 @@
+package br.com.erudio;
+
+public interface CreditCard {
+
+	public void giveBankDetails();
+	public String getCreditCard();
+}
