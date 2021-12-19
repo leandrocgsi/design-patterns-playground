66381293fbc8bf6ce522498e8a1788784f08419a
leandrocgsi <leandrocgsi@gmail.com>
2021-12-19 18:12:17 -0300

    Working with Strategy Pattern

---
+++ behavioral-design-patterns/strategy-pattern/src/main/java/br/com/erudio/Strategy.java
@@ -0,0 +1,6 @@
+package br.com.erudio;
+
+public interface Strategy {  
+    
+  public float calculation(float a, float b);  
+}
---
+++ behavioral-design-patterns/strategy-pattern/src/main/java/br/com/erudio/Subtraction.java
@@ -0,0 +1,9 @@
+package br.com.erudio;
+
+public class Subtraction implements Strategy {
+
+	@Override
+	public float calculation(float a, float b) {
+		return a - b;
+	}
+}
