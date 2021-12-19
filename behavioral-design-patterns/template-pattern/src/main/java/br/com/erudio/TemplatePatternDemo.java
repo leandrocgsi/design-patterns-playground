79fa7d8352d1e2bb48f7db52b4109f43c8ed4bc4
leandrocgsi <leandrocgsi@gmail.com>
2021-12-19 18:27:05 -0300

    Working with Template Pattern

---
+++ behavioral-design-patterns/template-pattern/src/main/java/br/com/erudio/TemplatePatternDemo.java
@@ -0,0 +1,12 @@
+package br.com.erudio;
+
+public class TemplatePatternDemo {
+	
+	public static void main(String[] args)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+
+		Class c = Class.forName(args[0]);
+		Game game = (Game) c.newInstance();
+		game.play();
+	}
+}
