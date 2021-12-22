6552c2aa80ead10b5bf468b93b964c876a5330fb
leandrocgsi <leandrocgsi@gmail.com>
2021-12-22 10:54:01 -0300

    Working with Flyweight Pattern

--- structural-design-patterns/flyweight-pattern/src/main/java/br/com/erudio/PaintBrushClient.java
+++ structural-design-patterns/flyweight-pattern/src/main/java/br/com/erudio/PaintBrushClient.java
@@ -1,7 +1,7 @@
 package br.com.erudio;
 
 public class PaintBrushClient {
-	
+
 	public static void main(String[] args) {
 		Pen yellowThinPen1 = PenFactory.getThickPen("YELLOW"); // created new pen
 		yellowThinPen1.draw("Hello World !!");
